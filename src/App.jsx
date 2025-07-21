8b8185856d5213b4c200f17e4628863769b105fb
ThalizFajardo <fajardothaliz@gmail.com>
2025-07-21 11:06:04 -0600

    First commit with base structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Menu from "./components/Shared/Menu";
+import './styles/main.css'
+
+
+
+function App() {
+  return (
+    <>
+      <Menu />
+      <Home />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <ContactMe />
+    </>
+  );
+}
+
+export default App;
