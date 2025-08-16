1073dce4b3ede3d066e84ed2496b442c2e33d857
ThalizFajardo <fajardothaliz@gmail.com>
2025-08-15 19:46:40 -0600

    fb1

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import mappinIcon from "../assets/map-pin.svg"
-import filbadgeIcon from "../assets/file-badge.svg"
-import graduationIcon from "../assets/graduation-cap.svg"
-import mailIcon from "../assets/mail.svg"
-import phoneIcon from "../assets/phone.svg"
-import profilepic from "../assets/pics/profilepic.jpg"
-
+import mappinIcon from "../assets/map-pin.svg";
+import filbadgeIcon from "../assets/file-badge.svg";
+import graduationIcon from "../assets/graduation-cap.svg";
+import mailIcon from "../assets/mail.svg";
+import phoneIcon from "../assets/phone.svg";
+import profilepic from "../assets/pics/profilepic.jpg";
 
 const AboutMe = () => {
   return (
@@ -19,10 +18,13 @@ const AboutMe = () => {
           <div className="about-inner">
             <div className="about-img-container">
               <div className="about-img-border">
-                <figure className="about-img-inner">
+                <figure
+                  className="about-img-inner"
+                  style={{ backgroundImage: `url(${profilepic})` }}  >
                   <img
                     src={profilepic}
-                    alt="profile picture"
+                    alt="Profile picture"
+                    className="profile-img"
                   />
                 </figure>
               </div>
@@ -37,14 +39,21 @@ const AboutMe = () => {
 
               <div className="about-info-text">
                 <p>
-                  Hi i'm <span> Thaliz Fajardo</span> I'm a frontend-focused developer with experience leading high-impact digital projects and coordinating technical teams. I'm passionate about building functional, automated, and scalable solutions—especially when they solve real business challenges. I enjoy collaborating with multidisciplinary teams, providing technical vision, and bringing structure and efficiency through development. Currently, I’m expanding my skill set toward full-stack development to increase my technical impact and autonomy when creating solutions.
+                  Hi i'm <span> Thaliz Fajardo</span> I'm a frontend-focused
+                  developer with experience leading high-impact digital projects
+                  and coordinating technical teams. I'm passionate about
+                  building functional, automated, and scalable
+                  solutions—especially when they solve real business challenges.
+                  I enjoy collaborating with multidisciplinary teams, providing
+                  technical vision, and bringing structure and efficiency
+                  through development. Currently, I’m expanding my skill set
+                  toward full-stack development to increase my technical impact
+                  and autonomy when creating solutions.
                 </p>
               </div>
 
               <div className="about-info-list">
                 <ul>
-                  
-            
                   <li>
                     <span className="about-info-logo">
                       <img src={mappinIcon} alt="Location Icon" />
@@ -56,7 +65,7 @@ const AboutMe = () => {
 
                   <li>
                     <span className="about-info-logo">
-                     <img src={filbadgeIcon} alt="Study Icon" />
+                      <img src={filbadgeIcon} alt="Study Icon" />
                     </span>
                     <span>
                       <label>Study:</label>Academlo
@@ -65,7 +74,7 @@ const AboutMe = () => {
 
                   <li>
                     <span className="about-info-logo">
-                     <img src={graduationIcon} alt="graduation Icon" />
+                      <img src={graduationIcon} alt="graduation Icon" />
                     </span>
                     <span>
                       <label>Degree:</label>Bachelor
@@ -78,7 +87,9 @@ const AboutMe = () => {
                     </span>
                     <span>
                       <label>Mail:</label>
-                      <a href="mailto:fajardothaliz@gmail.com">fajardothaliz@gmail.com</a>
+                      <a href="mailto:fajardothaliz@gmail.com">
+                        fajardothaliz@gmail.com
+                      </a>
                     </span>
                   </li>
 
